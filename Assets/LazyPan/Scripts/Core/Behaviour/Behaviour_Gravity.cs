0f032232197420f32a5c6a0531f84c738b6f0869
Evoreek <Evoreek@outlook.com>
2023-11-13 18:33:41 +0800

    提交

---
+++ Assets/LazyPan/Scripts/Core/Behaviour/Behaviour_Gravity.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace LazyPan {
+    public class Behaviour_Gravity : Behaviour {
+        private float GravityValue;
+        private Vector3 moveDir;
+        private CharacterController controller;
+
+        public Behaviour_Gravity(int subjectId) : base(subjectId) {
+            Data.Instance.OnUpdateEvent?.AddListener(Update_Behaviour_Gravity);
+            controller = SubjectGo.UGo.GetComponent<Comp>().Get<CharacterController>("CharacterController");
+            GravityValue = float.Parse(BehaviourConfig.Get("Behaviour_Gravity").Parameter);
+        }
+
+        private void Update_Behaviour_Gravity() {
+            if (controller != null) {
+                if (controller.gameObject.transform.position.y <= 0) {
+                    moveDir.y = 0f;
+                }
+                moveDir.y += -GravityValue * Time.deltaTime;
+                controller.Move(moveDir * Time.deltaTime);
+            }
+        }
+    }
+}
--- Assets/LazyPan/Scripts/Core/Behaviour/Behaviour_Jump.cs
+++ Assets/LazyPan/Scripts/Core/Behaviour/Behaviour_Jump.cs
@@ -5,6 +5,7 @@ namespace LazyPan {
     public class Behaviour_Jump : Behaviour {
         private Vector3 moveDir;
         private CharacterController controller;
+
         public Behaviour_Jump(int subjectId) : base(subjectId) {
             Input.Instance.Load("Player/Jump", Input_Jump);
             Data.Instance.OnUpdateEvent?.AddListener(Update_Behaviour_Jump);
@@ -13,13 +14,14 @@ namespace LazyPan {
 
         private void Input_Jump(InputAction.CallbackContext callbackContext) {
             if (callbackContext.performed) {
-                Debug.Log("jump");
-                moveDir.y += Mathf.Sqrt(5 * -3.0f * 5);
+                moveDir.y += Mathf.Sqrt(50 * -3.0f * 10f);
             }
         }
 
         private void Update_Behaviour_Jump() {
-            controller.Move(moveDir * Time.deltaTime);
+            if (controller != null && moveDir != Vector3.zero) {
+                controller.Move(moveDir * Time.deltaTime);
+            }
         }
     }
 }
