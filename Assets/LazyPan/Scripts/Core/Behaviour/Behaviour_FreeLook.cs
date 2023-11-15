26c0df6856b1f641cddd389cdbe1530a4a9f2869
Evoreek <wh_961230@163.com>
2023-11-15 23:19:54 +0800

    提交

--- Assets/LazyPan/Scripts/Core/Behaviour/Behaviour_FreeLook.cs
+++ Assets/LazyPan/Scripts/Core/Behaviour/Behaviour_FreeLook.cs
@@ -6,11 +6,10 @@ namespace LazyPan {
         private GameObject targetUgo;
 
         public Behaviour_FreeLook(int subjectId) : base(subjectId) {
-            if (Data.Instance.TryGetDataBodyByType((int)GoType.Camera, out int cameraId)) {
-                targetUgo = Data.Instance.dataBodyDic[cameraId].Go.UGo;
-
-                targetUgo.AddComponent<CinemachineBrain>();
-                CinemachineFreeLook cinemachineFreeLook = targetUgo.AddComponent<CinemachineFreeLook>();
+            SubjectGo.UGo.AddComponent<CinemachineBrain>();
+            CinemachineFreeLook cinemachineFreeLook = SubjectGo.UGo.AddComponent<CinemachineFreeLook>();
+            if (Data.Instance.TryGetDataBodyByType((int)GoType.MainPlayer, out int ID)) {
+                targetUgo = Data.Instance.dataBodyDic[ID].Go.UGo;
                 cinemachineFreeLook.LookAt = targetUgo.transform;
                 cinemachineFreeLook.Follow = targetUgo.transform;
             }
