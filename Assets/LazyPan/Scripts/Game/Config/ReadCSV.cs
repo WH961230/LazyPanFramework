ff5fbb6ee2b5508e4aff5dbfd992bdcd4ee51783
Evoreek <Evoreek@outlook.com>
2023-11-17 10:35:44 +0800

    提交

--- Assets/LazyPan/Scripts/Game/Config/ReadCSV.cs
+++ Assets/LazyPan/Scripts/Game/Config/ReadCSV.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ReadCSV : Singleton<ReadCSV> {
     public void Read(string fileName, out string content, out string[] lines) {
-        using (StreamReader sr = new StreamReader(Application.streamingAssetsPath + "\\" + "Csv\\" + fileName + ".csv")) {
+        using (StreamReader sr = new StreamReader(Application.streamingAssetsPath + "/Csv/" + fileName + ".csv")) {
             string str = null;
             string line;
             while ((line = sr.ReadLine()) != null) {
